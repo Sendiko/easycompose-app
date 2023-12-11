7770a1a49b3b238daf3be067d60f0a33e1ec1a1a
Sendiko <sarangtawon897@gmail.com>
2023-12-11 23:39:49 +0700

    enc(crea): CustomTextField

---
+++ app/src/main/java/com/sendiko/easycompose/ui/components/CustomTextField.kt
@@ -0,0 +1,43 @@
+package com.sendiko.easycompose.ui.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    isPassword: Boolean = false,
+    isEnabled: Boolean = true,
+    leadingIcon: ImageVector,
+    trailingIcon: @Composable () -> Unit
+) {
+    OutlinedTextField (
+        modifier = modifier,
+        enabled = isEnabled,
+        value = value,
+        onValueChange = {
+            onValueChange(it)
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = leadingIcon,
+                contentDescription = null
+            )
+        },
+        trailingIcon = trailingIcon,
+        visualTransformation = if (isPassword)
+            PasswordVisualTransformation()
+        else VisualTransformation.None,
+        shape = RoundedCornerShape(100)
+    )
+}
