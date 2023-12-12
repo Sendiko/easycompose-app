67347d378ba954b20353f33a14c1f1f54aa364c4
Sendiko <sarangtawon897@gmail.com>
2023-12-12 08:23:49 +0700

    enc(add): hint for TextField

--- app/src/main/java/com/sendiko/easycompose/ui/components/CustomTextField.kt
+++ app/src/main/java/com/sendiko/easycompose/ui/components/CustomTextField.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -19,12 +20,16 @@ fun CustomTextField(
     isPassword: Boolean = false,
     isEnabled: Boolean = true,
     leadingIcon: ImageVector,
+    hint: String,
     trailingIcon: @Composable () -> Unit
 ) {
     OutlinedTextField (
         modifier = modifier,
         enabled = isEnabled,
         value = value,
+        placeholder = {
+            Text(text = hint)
+        },
         onValueChange = {
             onValueChange(it)
         },
